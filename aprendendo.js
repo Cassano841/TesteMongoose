4ac7dfbd03ace5f3e7b60758fe475338d4187a96
Cassano <ninocb@gmail.com>
2022-01-30 17:26:26 -0300

    first commit

---
+++ aprendendo.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/aprendendo", {
+  })
+  .then(() => {
+    console.log("MongoDB conectado...");
+  })
+  .catch((err) => {
+    console.log("Hou um erro ao se conectar ao MongoDB..." + err);
+  });
+
+const UserSchema = mongoose.Schema({
+  nome: {
+    type: String,
+    require: true,
+  },
+  sobrenome: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  idade: {
+    type: Number,
+    require: true,
+  },
+  pais: {
+    type: String,
+  },
+});
+
+mongoose.model("Users", UserSchema);
+
+const NovoUsuario = mongoose.model("Users");
+
+new NovoUsuario({
+  nome: "Aline",
+  sobrenome: "Dias",
+  email: "teste@etste",
+  idade: 22,
+  pais: "Brasil",
+})
+  .save()
+  .then(() => {
+    console.log("Usuário cadastrado");
+  })
+  .catch((err) => {
+    console.log("Houve um erro ao cadastrado o usuário" + err);
+  });
